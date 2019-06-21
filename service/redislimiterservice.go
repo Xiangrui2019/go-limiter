61c3d107f26aee299ef9c4069d3b7933c3f0b8e1
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-06-21 13:42:41 +0800

    sdfaaffafds

---
+++ service/redislimiterservice.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/xiangrui2019/redis"
+	"strconv"
+)
+type RedisLimiterService struct {
+	ctx context.Context
+	client redis.Client
+}
+
+func (service *RedisLimiterService) Limit(serviceName string, clientid string, limit int64, duration int32) error {
+	var sum int64
+	var err error
+
+	data, err := service.client.Get(service.ctx, serviceName + "&-&" +clientid)
+
+	if data == nil {
+		sum = 1
+		sumstring := strconv.Itoa(int(sum))
+
+		err := service.client.Set(service.ctx, &redis.Item{
+			Key:   serviceName + "&-&" + clientid,
+			Value: []byte(sumstring),
+			TTL:   duration,
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		return nil
+	} else {
+		sum, err = strconv.ParseInt(string(data.Value), 10, 0)
+
+		if err != nil {
+			return err
+		}
+
+		if sum >= limit {
+			return errors.New("limit too big.")
+		} else {
+			sum, err = service.client.IncrBy(service.ctx, serviceName + "&-&" + clientid, 1)
+
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	return nil
+}
